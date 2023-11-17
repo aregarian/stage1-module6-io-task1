d7715159f669444838f497751c35eb57f68afa5e
Fanny Dorthey <fannydor@un-k4.kzn.21-school.ru>
2023-11-17 04:22:50 +0300

    heheheh boyoyoyoy

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -15,35 +15,34 @@ public class FileReader {
         int age = 4;
         String email = null;
 
+
         StringBuilder profileData = new StringBuilder();
-        try {
-            InputStream stream = new FileInputStream(file);
+        try (FileInputStream stream = new FileInputStream(file)) {
             int i;
             while ((i = stream.read()) != -1) {
                 profileData.append((char) i);
             }
-            stream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+            String string = profileData.toString();
+            String[] splitStrings = string.split(": ");
+            name = splitStrings[1];
+            age = Integer.parseInt(splitStrings[2].replaceAll("[^0-9,]", ""));
+            String[] hehe = name.split("[^\\w']+");
+            String[] hehe1 = hehe[0].split("Age");
+            String hehegirl = hehe1[0];
 
-        String string = profileData.toString();
-        String[] splitStrings = string.split(": ");
-        name = splitStrings[1];
-        age = Integer.parseInt(splitStrings[2].replaceAll("[^0-9,]",""));
-        String[] hehe = name.split("[^\\w']+");
-        String[] hehe1 = hehe[0].split("Age");
-        String hehegirl = hehe1[0];
 
+            Profile profile = new Profile(
+                    hehegirl,
+                    age,
+                    hehegirl,
+                    (long) age
+            );
+            return profile;
 
-        Profile profile = new Profile(
-                hehegirl,
-                age,
-                hehegirl,
-                (long) age
-        );
-        return profile;
-
+        }
     }
-}
+
